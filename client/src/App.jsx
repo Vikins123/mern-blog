0d38b5d1ea96d1d0f5e56fed512454aa84ac2191
Vikns <vishalmuchhdiya87@gmail.com>
2024-02-13 15:25:53 +0530

    install react is and tailwind css and create the  first template

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-7xl text bg-yellow-400'>mayur</h1>
+      <h2 className='text-9xl text bg-blue-600'>muchhdiya</h2>
+    </>
+  )
+}
+
+export default App
